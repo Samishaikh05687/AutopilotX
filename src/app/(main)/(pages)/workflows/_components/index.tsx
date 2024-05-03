9a3c8ea178361250a40d2b406bbb4b95cc74a083
sami05687 <samishaikh05687@gmail.com>
2024-05-04 01:36:40 +0530

    complete token generation

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Workflow from './workflow'
+import { onGetWorkflows } from '../_actions/workflow-connections'
 
 type Props = {}
 
-const Workflows = (props: Props) => {
+const Workflows = async (props: Props) => {
+  const workflows = await onGetWorkflows()
   return (
     <div className='relative flex flex-col gap-4 '>
         <section className='flex flex-col gap-4 m-2'>
-            <Workflow 
-            description='Creating a test workflow'
-            id='e23498fj23948n'
-            name='Automation Workflow'
-            publish={false}
-            />
+            {workflows?.length ? workflows.map((flow) => (
+              <Workflow 
+              key={flow.id}
+              {...flow}
+              />
+            ))
+          : (
+            <div className='mt-28 flex items-center justify-center text-muted-foreground'> No Workflows</div>
+          )}   
         </section>
     </div>
   )
